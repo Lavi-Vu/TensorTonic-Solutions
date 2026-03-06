fbb0882acf7bb397af565d01a21c20ce8a7df5c3
Lavi-Vu <69051857+Lavi-Vu@users.noreply.github.com>
2026-03-06 17:15:22 +0700

    Add solution for color-to-grayscale

---
+++ color-to-grayscale/color-to-grayscale.py
@@ -0,0 +1,11 @@
+def color_to_grayscale(image):
+    """
+    Convert an RGB image to grayscale using luminance weights.
+    """
+    # Write code here
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            r, g, b = image[i][j]
+            grayscale_value = 0.299 * r + 0.587 * g + 0.114 * b
+            image[i][j] = grayscale_value
+    return image
